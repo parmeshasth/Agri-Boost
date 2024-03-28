e702454d50af645ca9c686b4c3e02137ef4488cd
KumarVK <128793825+AlokPandey123@users.noreply.github.com>
2024-03-28 21:26:15 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const slides=document.querySelectorAll(".slide");
+var counter = 0;
+slides.forEach((slide,index)=>{
+    slide.style.left=`${index * 100}%`
+});
+
+const slideImage=()=>{
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform=`transaltX(${counter*100}%)`
+        }
+    )
+}
